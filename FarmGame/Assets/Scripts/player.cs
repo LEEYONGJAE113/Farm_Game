79ad0cb69f95ed65e1f0d9dfa7ca7f4a5846cf46
DESKTOP-SCTFQSJ\A <ionsi1103@gmail.com>
2025-01-26 01:35:19 +0900

    이동 체크함

--- FarmGame/Assets/Scripts/player.cs
+++ FarmGame/Assets/Scripts/player.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class player : MonoBehaviour
 {
+    private Vector2 _inputVec;
+    private Rigidbody2D _rb;
 
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = _inputVec * 3/*movespeed*/ * Time.fixedDeltaTime;
+        Vector2 nextPos = _rb.position + nextVec;
+
+        _rb.MovePosition(nextPos);
+        _rb.linearVelocity = Vector2.zero;
+    }
+
+
+    void OnMove(InputValue iValue)
+    {
+        _inputVec = iValue.Get<Vector2>();
+    }
 }
